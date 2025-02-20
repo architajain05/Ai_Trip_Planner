ed287159b5f214c67598ba83be947584d67f1f7b
Archita <122664337+architajain05@users.noreply.github.com>
2025-02-20 18:11:15 +0000

    setup done

---
+++ trip-planner/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-5xl text-center p-16'>Welcome to SafarMate</h1>
+   <Button>Subscribe</Button>
+    </>
+  )
+}
+
+export default App
